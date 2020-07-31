417ae7245aec09a43f35cf4f4e480b732b7120f3
汉军 陈 <2658653217@qq.com>
2020-07-31 10:01:06 +0800

    friend

--- head.h
+++ head.h
@@ -2,15 +2,19 @@
 #include<string>
 #include<vector>
 using namespace std;
-struct Sales_data;
+struct Sales_data;//6666666
 istream& read(istream& is, Sales_data& item);//必须提前声明Sales_data
-struct Sales_data {
+class Sales_data {
+	friend istream& read(istream& is, Sales_data& item);
+	friend ostream& print(ostream& os, const Sales_data& item);
+public:
 	Sales_data() = default;
 	Sales_data(const string& s) : bookNo(s) {}
 	Sales_data(const string& s, unsigned n, double p) :bookNo(s), units_sold(n), revenue(p* n) {}
 	Sales_data(istream& is) { read(is, *this); };
 	string isbn() const { return bookNo; }
 	Sales_data& combine(const Sales_data&);
+private:
 	double avg_price() const;
 	string bookNo;
 	unsigned units_sold = 0;
